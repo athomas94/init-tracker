ffb78093d658742daa6e56c4bd5830e5c28dcef5
Andrew Thomas <athomas94@gmail.com>
2024-11-07 19:45:19 -0500

    Continuing work on setting up Postgres connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { Client } = require('pg');
 
 const app = express();
 
+//Middleware
 app.use(express.json());
 app.use(cors());
+
+//Postgres Connection
+const client = new Client({
+    connectionString: process.env.DB_CONNECTIONSTRING
+});
+client.connect() 
+    .then(() => {console.log('Connected to PostGres')
+    .catch((err) => console.error("Connection error", err.stack));;
+});
 
 app.get('/message', (req, res) => {
     res.json({message: 'Success From Server'});    
 });
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
+app.listen(process.env.PORT, () => {
+    console.log('Server started on port ${process.env.PORT}');
 });
